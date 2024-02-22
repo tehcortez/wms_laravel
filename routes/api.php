7398410b4b24217be32800163d0d29f678e62238
lucas cortez <lucas.cortez@w3americas.com>
2024-02-22 17:24:41 -0300

    add routes definition and routes versioning

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\V1\CustomerController;
+use App\Http\Controllers\Api\V1\InventoryController;
 use App\Http\Controllers\Api\V1\LineItemController;
 use App\Http\Controllers\Api\V1\OrdersController;
+use App\Http\Controllers\Api\V1\ProductController;
+use App\Http\Controllers\Api\V1\WarehouseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,7 +29,10 @@ Route::group(
     ['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'],
     function (){
         Route::apiResource('customer', CustomerController::class);
-        Route::apiResource('orders', OrdersController::class);
+        Route::apiResource('inventory', InventoryController::class);
         Route::apiResource('line-item', LineItemController::class);
+        Route::apiResource('orders', OrdersController::class);
+        Route::apiResource('product', ProductController::class);
+        Route::apiResource('warehouse', WarehouseController::class);
     }
 );
