57a4057c60b04146becfd4bf01a4239e2ec93d06
lucas cortez <lucas.cortez@w3americas.com>
2024-02-23 05:53:39 -0300

    add service to calculate ready_to_ship attribute

---
+++ app/Services/V1/ReadyToShipService.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Services\V1;
+
+use App\Models\Order;
+use App\Models\Product;
+
+class ReadyToShipService {
+    function updateAllReadyToShipFlags():void {
+        $productAvailabilityMap = $this->getProductAvailabilityMap();
+        $ordersWithLineItems = Order::all();
+        foreach ($ordersWithLineItems as $order){
+            $productAvailabilityMap = $this->updateProductAvailabilityMap($productAvailabilityMap, $order);
+        }
+    }
+
+    function getQuantityAvailable(Product $product) {
+        $quantityAvailable = 0;
+        foreach($product->inventories as $inventory){
+            $quantityAvailable += $inventory->quantity_available;
+        }
+        return $quantityAvailable;
+    }
+
+    function getProductAvailabilityMap():array {
+        $productsWithInventories = Product::all();
+        $productAvailabilityMap = [];
+        foreach ($productsWithInventories as $product){
+            $productAvailabilityMap[$product->product_id] = $this->getQuantityAvailable($product);
+        }
+        return $productAvailabilityMap;
+    }
+
+    function updateProductAvailabilityMap(array $productAvailabilityMap, Order $order): array {
+        $readyToShip = true;
+        foreach($order->lineItems as $lineItem){
+            if(!isset($productAvailabilityMap[$lineItem->product_id])){
+                $readyToShip = false;
+                continue;
+            }
+            $productAvailabilityMap[$lineItem->product_id] -= $lineItem->quantity;
+            if($productAvailabilityMap[$lineItem->product_id] < 0){
+                $readyToShip = false;
+            }
+        }
+        $order->ready_to_ship = $readyToShip;
+        $order->update(['ready_to_ship'=>$readyToShip]);
+        return $productAvailabilityMap;
+    }
+}
