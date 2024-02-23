0f6827fe2af6bff27e051303e9a42d3a2ed04037
lucas cortez <lucas.cortez@w3americas.com>
2024-02-22 22:42:12 -0300

    add resource and collection to all models

---
+++ app/Http/Resources/V1/OrderResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'orderId' => $this->order_id,
+            'customerId' => $this->customer_id,
+            'totalPrice' => $this->total_price,
+            'readyToShip' => $this->ready_to_ship,
+        ];
+    }
+}
